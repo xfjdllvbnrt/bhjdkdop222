f91cc831962ea5389f567cb6f523b2e0f3bbb51b
xfjdllvbnrt <83075885+xfjdllvbnrt@users.noreply.github.com>
2021-09-16 13:32:28 +0700

    Update update.sc-server.sh

--- update.sc-server.sh
+++ update.sc-server.sh
@@ -5,19 +5,19 @@ green='\e[0;32m'
 blue='\e[1;34m'
 NC='\e[0m'
 versi=$(cat /home/ver)
-if [ $versi == 3.4 ]; then
+if [ $versi == 3.5 ]; then
 echo -e "${blue}You Have The Latest Version${NC}"
 sleep 5
 exit 0
 else
 rm -f /root/log-update.txt
 echo -e "==================================="
-echo -e "${green}Update Avaible to 3.4{NC}"
+echo -e "${green}Update Avaible to 3.5${NC}"
 echo -e "  "
 echo -e " LOG UPDATE" | tee -a log-update.txt
 echo -e " " | tee -a log-update.txt
-echo -e " Ver 3.4" | tee -a log-update.txt
-echo -e " - Add SSH WS SSL" | tee -a log-update.txt
+echo -e " Ver 3.5" | tee -a log-update.txt
+echo -e " - Add Autorestart for WS-SSL" | tee -a log-update.txt
 echo -e " " | tee -a log-update.txt
 sleep 1
 echo -e "==================================="
@@ -186,14 +186,17 @@ END
 systemctl daemon-reload
 systemctl enable proxynode.service
 systemctl restart proxynode.service
-
-echo "3.4" > /home/ver
+cat << EOF >> /etc/crontab
+# BEGIN_Autorestart
+13 */12 * * * root systemctl restart proxynode
+# END_Autorestart
+EOF
+echo "3.5" > /home/ver
 fi
 clear
 echo -e "========================"
 echo -e "  Update Complete"
-echo -e "  Go to home and reboot in 5s"
+echo -e "  Go to home in 5s"
 echo -e "========================"
 sleep 5
 home
-reboot
